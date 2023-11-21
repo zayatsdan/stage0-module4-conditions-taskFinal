ee78f514627b49a80f2d91c94c76fe6710e23532
zayatsdan <36342659+zayatsdan@users.noreply.github.com>
2023-11-21 13:32:16 +0100

    Update CoordinatePane.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+        String place;
+       if (y==0||x==0){
+           place="zero";
+       }else {
+           place=(x>0&&y>0)?
+                 "first":
+                 x>0&&y<0?
+                 "second":
+                 x<0&&y<0?
+                 "third":
+                 "forth";
+       }
+        System.out.println(place);
     }
 }
